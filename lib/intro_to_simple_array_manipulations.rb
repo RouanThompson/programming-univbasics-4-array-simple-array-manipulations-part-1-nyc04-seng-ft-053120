bf9a22f2a64f6817a6aa587e650448f1ec5f9bea
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-08 19:51:28 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string_to_end)
+  array.push(string_to_end)
+end
+
+def using_unshift(array, string_to_front)
+  array.unshift(string_to_front)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
