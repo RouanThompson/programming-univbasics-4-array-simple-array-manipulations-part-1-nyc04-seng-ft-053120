db14306631c60d31e25855c3fe2c734cb2092463
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-08 19:45:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,7 @@
-def using_push(array, string_to_add)
-  array.push(string_to_add)
+def using_push(array, string_to_end)
+  array.push(string_to_end)
 end
 
-def using_unshift(array, string_to_add)
-  array.unshift(string_to_add)
+def using_unshift(array, string_to_front)
+  array.unshift(string_to_front)
 end
